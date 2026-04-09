8e6e753333346b51102198389933abf0721be454
Graham Newman <graham@innovationdesign.io>
2026-04-09 17:18:48 +0100

    Update header: G100 background and two-line title

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -4,9 +4,12 @@ import { Sun, Moon } from '@carbon/react/icons'
 
 export default function SiteHeader({ dark, onToggleTheme }) {
   return (
-    <Header aria-label="Reflexive practice">
+    <Header aria-label="Reflexive practice" style={{ background: '#161616' }}>
       <HeaderName href="#" prefix="">
-        Coded research journal
+        <div style={{ display: 'flex', flexDirection: 'column', lineHeight: 1 }}>
+          <span style={{ fontWeight: 400, fontSize: '1rem' }}>Graham Newman</span>
+          <span style={{ fontWeight: 700, fontSize: '1.125rem' }}>RCA PhD coded research journal</span>
+        </div>
       </HeaderName>
 
       <HeaderGlobalBar>
